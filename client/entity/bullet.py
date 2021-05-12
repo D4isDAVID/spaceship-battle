f01ec36020f6f7ba4e5e9fdf7e511e90f74054fe
David <malchin459@gmail.com>
2021-05-12 19:54:51 +0300

    Bullet Outline

--- client/entity/bullet.py
+++ client/entity/bullet.py
@@ -8,4 +8,5 @@ class BulletEntity:
         y = self.y - entity.y + height/2
         if entity == self.entity: self.color = (0, 0, 255)
         else: self.color = (255, 0, 0)
+        pygame.draw.circle(surface, (255, 255, 255), (x, y), self.radius+2, 1)
         pygame.draw.circle(surface, self.color, (x, y), self.radius)
